f10ef7acf25d1ea4c2dc13c1b0670dde292caa00
Malek Mansour <mansour_malek@me.com>
2025-11-17 09:43:30 +0100

    first commit
    project creation

---
+++ FDJ overview/FDJ_overviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  FDJ_overviewApp.swift
+//  FDJ overview
+//
+//  Created by Malek Mansour on 17/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct FDJ_overviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FDJ overviewTests/FDJ_overviewTests.swift
@@ -0,0 +1,17 @@
+//
+//  FDJ_overviewTests.swift
+//  FDJ overviewTests
+//
+//  Created by Malek Mansour on 17/11/2025.
+//
+
+import Testing
+@testable import FDJ_overview
+
+struct FDJ_overviewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
